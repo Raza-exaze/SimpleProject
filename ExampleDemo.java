eab5b11284cb719334dfadaf29332bd2488e02f0
Raza7860 <kingofkingraza1@gmail.com>
2022-02-08 11:32:52 +0530

    Second Commit

---
+++ ExampleDemo.java
@@ -0,0 +1,13 @@
+public class ExampleDemo {
+    public static void main(String args[])
+    {
+        System.out.println("Hello World");
+
+            for (int i = 1; i <= "Hello".length(); i++) {
+                System.out.print(i+" ");
+            }
+            String a="Hello";
+            char c=a.charAt(0);
+            System.out.println(c);
+    }
+}
